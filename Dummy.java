6c3ad3aad550ee010032c4267fc9b6102925865a
kavyabs18 <143055412+kavyabs18@users.noreply.github.com>
2023-11-18 21:45:02 +0530

    Add files via upload

---
+++ Dummy.java
@@ -0,0 +1,10 @@
+public class Dummy {
+public static void main(String[] args) {
+	int a=10;
+	System.out.println(a+=5);
+	System.out.println(a-=3);
+	System.out.println(a*=2);
+	System.out.println(a/=6);
+	System.out.println(a%=3);
+}
+}
