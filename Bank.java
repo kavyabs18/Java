9a70b20b03c0fa310534072cc9a9c3a1a6de8716
kavyabs18 <143055412+kavyabs18@users.noreply.github.com>
2023-12-20 20:59:22 +0530

    Add files via upload

--- Bank.java
+++ Bank.java
@@ -1,15 +1,24 @@
-package com.kodnest.banking;
+package com.kodnest.exception_handling;
+
+import java.util.Scanner;
 
 public class Bank {
-	public int getHeromoney()
-	{
-		return heromoney;
-	}
-	
-	public void setHeromoney(int heromoney)
-	{
-		this.heromoney = heromoney;
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the amount for withdrawl");
+		int amount = scan.nextInt();
+		try {
+			withdraw(amount);
+		} catch (MyException e) {
+//			e.printStackTrace();
+			System.out.println(e);
+		}
 	}
 
-	private int heromoney = 10000;
+	static void withdraw(int amount) throws MyException {
+		int balance = 10000;
+		if(amount>balance) {
+			throw new MyException();
+		}
+	}
 }
