dfd14e8aa66290c737afc66ae9d51b06e82260e6
kavyabs18 <143055412+kavyabs18@users.noreply.github.com>
2024-01-02 21:24:46 +0530

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,26 @@
+package com.kodnest.thread;
+
+public class Main2 {
+	public static void main(String[] args) {
+		Thread.currentThread().setPriority(8);
+		System.out.println(Thread.currentThread());
+		
+		for(int i=0; i<=10; i++) {
+			if(i%2!=0)
+			System.out.println(i);
+		}
+		
+		Task task = new Task();
+		task.start();
+	}
+}
+class Task extends Thread{
+	public void run(){
+		Thread.currentThread().setPriority(5);
+		System.out.println(Thread.currentThread());
+		for(int i=0; i<=10; i++) {
+			if(i%2==0)
+			System.out.println(i);
+		}
+	}
+}
