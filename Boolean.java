7418ede6055d0b55b3c0268f63245f07af6ccc18
kavyabs18 <143055412+kavyabs18@users.noreply.github.com>
2024-01-07 18:24:15 +0530

    Add files via upload

---
+++ Boolean.java
@@ -0,0 +1,10 @@
+class Boolean
+{
+public static void main(String[] args)
+{
+boolean a=true;
+boolean b=false;
+System.out.println("The Boolean value is "+a);
+System.out.println("The Boolean value is "+b);
+}
+}
