0960fb5e6acadbac980ddfa197e170b147ac5a1b
kavyabs18 <143055412+kavyabs18@users.noreply.github.com>
2024-02-02 20:18:50 +0530

    Add files via upload

--- Q2.java
+++ Q2.java
@@ -1,10 +1,19 @@
-package com.kodnest.loopingpractice;
+package com.kodnest.datatypes;
+
+import java.util.Scanner;
 
 public class Q2 {
 public static void main(String[] args) {
-	for(int i=1; i<=5; i++)
-	{
-		System.out.println("*");
-	}
+	/*Write a Java program that reads a number in inches and converts it to meters.
+Note: One inch is 0.0254 meter.
+Test Data
+Input a value for inch: 1000
+Expected Output :
+1000.0 inch is 25.4 meters*/
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter the value in inches");
+	float i = scan.nextFloat();
+	float m = i*0.0254f;
+	System.out.println(i+" inch is "+m+" meters");
 }
 }
