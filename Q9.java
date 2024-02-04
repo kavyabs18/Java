9bf56150a7ba0c8986b7baffe64a2ff964b24076
kavyabs18 <143055412+kavyabs18@users.noreply.github.com>
2024-02-04 20:19:06 +0530

    Add files via upload

--- Q9.java
+++ Q9.java
@@ -1,20 +1,38 @@
-package com.kodnest.loopingpractice;
+package com.kodnest.datatypes;
+
+import java.util.Scanner;
 
 public class Q9 {
 public static void main(String[] args) {
-	for(int i=1; i<=5; i++)
-	{
-		for(int j=1; j<=5; j++)
-		{
-			if(j%2==0&&i%2==0||j==3&&i==5||j==3&&i==1||i==3&&j==1||i==3&&j==5)
-			{
-				System.out.print("*");
-			}
-			else {
-				System.out.print(" ");
-			}
-		}
-		System.out.println();
+	/*Write a Java program that accepts two integers from the user and prints the sum, the difference, the product, the average, the distance (the difference between the integers), the maximum (the largest of the two integers), and the minimum (the smallest of the two integers).
+
+Test Data
+Input 1st integer: 25
+Input 2nd integer: 5
+Expected Output :
+Sum of two integers: 30
+Difference of two integers: 20
+Product of two integers: 125
+Average of two integers: 15.00
+Distance of two integers: 20
+Max integer: 25
+Min integer: 5*/
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Input 1st integer: ");
+	int a = scan.nextInt();
+	System.out.println("Input 2nd integer: ");
+	int b = scan.nextInt();
+	System.out.println("Sum of two integers: "+(a+b));
+	System.out.println("Difference of two integers: "+(a-b));
+	System.out.println("Product of two integers: "+(a*b));
+	System.out.println("Average of two integers: "+((a+b)/2));
+	System.out.println("Distance of two integers: "+(a-b));
+	if(a>b) {
+		System.out.println("Max integer: "+a);
+		System.out.println("Min integer: "+b);
+	}else {
+		System.out.println("Min integer: "+b);
+		System.out.println("Max integer: "+a);
 	}
 }
 }
