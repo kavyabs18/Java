f5740955cbcbd9908abd9c8810a0da388f640c4d
kavyabs18 <kavyabs93630@gmail.com>
2024-03-19 20:47:18 +0530

    HackerRank
    HackerRank Questions Solution

---
+++ HackerRank/Inheritance_1.java
@@ -0,0 +1,29 @@
+package com.kodnest.hackerrank;
+
+public class Inheritance_1 {
+
+	public static void main(String args[]) {
+
+		Bird bird = new Bird();
+		bird.walk();
+		bird.fly();
+		bird.sing();
+
+	}
+}
+
+class Animal {
+	void walk() {
+		System.out.println("I am walking");
+	}
+}
+
+class Bird extends Animal {
+	void fly() {
+		System.out.println("I am flying");
+	}
+
+	void sing() {
+		System.out.println("I am singing");
+	}
+}
