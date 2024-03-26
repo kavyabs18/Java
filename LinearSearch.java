e0cd1d161726b38937aa3e6e88b61bb867a13dde
kavyabs18 <143055412+kavyabs18@users.noreply.github.com>
2024-03-26 18:52:46 +0530

    Add files via upload

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,39 +1,34 @@
-package com.kodnest.array;
+package com.kodnest.practice;
 
 import java.util.Scanner;
 
 public class LinearSearch {
-public static void main(String[] args) {
-	Scanner scan = new Scanner(System.in);
-	
-	//size of an Array
-	System.out.println("User please enter the size of an Array");
-	int size = scan.nextInt();
-	
-	//creation of an Array
-	int a[] = new int[size];
-	
-	//read the elements
-	System.out.println("User please enter the elements");
-	for(int i=0; i<a.length; i++) {
-		a[i] = scan.nextInt();
-	}
-
-	//Search element
-	System.out.println("Enter the element to search");
-	int find = scan.nextInt();
-	boolean ispresent = false;
-	for(int i=0; i<a.length; i++) {
-		if(a[i]==find) {
-			ispresent = true;
-			break;
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the size of an Array");
+		int size = scan.nextInt();
+		int a[] = new int[size];
+		for(int i=0; i<a.length; i++) {
+			a[i] = scan.nextInt();
 		}
+		System.out.println("Enter the Target Element");
+		int target = scan.nextInt();
+		int result = LinearSearch(a,target);
+		if(result!=-1) {
+			System.out.println("Element "+target+" found at index "+result);
+		}else {
+			System.out.println("Element not found");
+		}
+		
 	}
-	if(ispresent ==true) {
-		System.out.println("Present");
+	static int LinearSearch(int[] a, int target) {
+		int i=0;
+		while(i<a.length) {
+			if(a[i]==target) {
+				return i;
+			}
+			i++;
+		}
+		return -1;
 	}
-	else {
-		System.out.println("Not Present");
-	}
-}
 }
